15bb98e71d1a2d65ca277a8f9e2e90259dc43cf8
Yanping Pei <146985677+yanpingPei@users.noreply.github.com>
2026-07-18 18:30:52 -0400

    Rename job and update script paths in run.sh

--- sims_yanping/turing/run.sh
+++ sims_yanping/turing/run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-#SBATCH --job-name="yhatX1RF"
+#SBATCH --job-name="nextSimTest"
 #SBATCH -D /home/ypei/obsReLOOP
-#SBATCH --output=logs/yhatX1RF_%A_%a.out
-#SBATCH --error=logs/yhatX1RF_%A_%a.err
+#SBATCH --output=logs/nextSimTest_%A_%a.out
+#SBATCH --error=logs/nextSimTest_%A_%a.err
 #SBATCH --array=1-50
 #SBATCH --time=24:00:00
 #SBATCH --cpus-per-task=1
@@ -14,6 +14,7 @@
 module purge
 module load r
 
-mkdir -p results
+mkdir -p logs
+mkdir -p results/next
 
-Rscript /home/ypei/obsReLOOP/yhatRF.R $SLURM_ARRAY_TASK_ID 50
+Rscript /home/ypei/obsReLOOP/nextSim.R $SLURM_ARRAY_TASK_ID 50
